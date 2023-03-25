6a5221cb569168364ff972ee1ccf15fba7e841cb
Nahom Abraham <nahom@kemercode.com>
2023-03-25 12:45:22 +0300

    Change the dice numbers to dots

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
 export default function Die(props){
+
+    function dotElements(){
+        let dots = []
+        for(let i = 0; i < props.value; i++){
+            dots.push(<div className="die-dots"></div>)
+        }
+        return dots
+    }
+
+
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
         <div className="dice" style={styles} onClick={props.holdDice}>
-            {props.value}
+            {dotElements()}
         </div>
     )
 }
