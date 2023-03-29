c344136ba288edf21204ceabad38f92988240a2c
Nahom Abraham <nahom@kemercode.com>
2023-03-29 11:58:40 +0300

    Store best time in local storage

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,12 +2,24 @@ import React from "react";
 
 export default function Timer(props){
     const [time, setTime] = React.useState(0)
+    const [bestTime, setBestTime] = React.useState(
+        JSON.parse(localStorage.getItem("best-time")) || "-"
+        )
     const [id, setId] = React.useState(0)
 
     React.useEffect(
         () => {
             if(props.tenzies){
                 clearInterval(id)
+                setBestTime(prevBestTime => {
+                    if (prevBestTime === "-" || time < prevBestTime){
+                        localStorage.setItem("best-time", JSON.stringify(time))
+                        return time
+                    }
+                    else {
+                        return prevBestTime
+                    }
+                })
             }
             else{
                 setId(setInterval(setCurrentTime, 1000))
@@ -25,14 +37,22 @@ export default function Timer(props){
         setTime(time => time+1)
     }
 
-    function formatTime(){
+    function format(time){
+        if (time === "-")
+            return time
         return `${Math.floor(time/60)}:${time%60}`
     }
     
     return (
-        <div className="timer">
-            <div className="timer-text">Time </div>
-            <div className="timer-time">{formatTime()}</div>
+        <div className="timer-container">
+            <div className="timer">
+                <div className="timer-text">Time</div>
+                <div className="timer-time">{format(time)}</div>
+            </div>
+            <div className="timer">
+                <div className="timer-text">Best</div>
+                <div className="timer-time">{format(bestTime)}</div>
+            </div>
         </div>
     )
 }
